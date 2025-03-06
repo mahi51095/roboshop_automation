c4fe67749df47427393b6c3281bc658e4fb8f94c
Mahesh <mahi51095@gmail.com>
2025-03-06 18:08:09 +0530

    Added Redis

---
+++ web.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/home/centos/roboshop_automation
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ]
+then
+    echo -e  "$R ERROR : Please run script with root access. $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 .... $R FAILURE $N"
+        
+    else
+        echo -e "$2 .... $G SUCCESS $N"
+    fi
+}
+
+yum install nginx -y &>>$LOGFILE
+
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx &>>$LOGFILE
+
+VALIDATE $? "Enabling Nginx"
+
+systemctl start nginx &>>$LOGFILE
+
+VALIDATE $? "Starting Nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+
+VALIDATE $? "Removing default html file"
+
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>>$LOGFILE
+
+VALIDATE $? "Downloading web artifact"
+
+cd /usr/share/nginx/html &>>$LOGFILE
+
+VALIDATE $? "Moving to default html directory"
+
+unzip /tmp/web.zip &>>$LOGFILE
+
+VALIDATE $? "Unzipping web artifact"
+
+cp /home/centos/roboshop_automation /etc/nginx/default.d/roboshop.conf &>>$LOGFILE
+
+VALIDATE $? "Copying roboshop config"
+
+systemctl restart nginx &>>$LOGFILE
+
+VALIDATE $? "Restarting nginx"
