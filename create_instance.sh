f493d34772e97e89744e3f0fe3e66a5821064955
Mahesh <mahi51095@gmail.com>
2025-03-10 19:06:37 +0530

    Added ec2.sh

--- create_instance.sh
+++ create_instance.sh
@@ -9,7 +9,7 @@ SECURITY_GROUP_ID=sg-0de06c7e419c50b0c
 
 for i in "${NAMES[@]}"
 do 
-   if [ $i == "mongodb" || $i == "mysql" ]
+   if [ $i == "mongodb" || $i == "mysql" ];
    then
         INSTANCE_TYPE="t3.medium"
     else
