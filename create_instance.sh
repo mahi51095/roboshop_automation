5ce50b0858bb2beedf6fad77fe3ea06f8c6246d9
Mahesh <mahi51095@gmail.com>
2025-03-10 19:05:49 +0530

    Added ec2.sh

--- create_instance.sh
+++ create_instance.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+INSTANCE_TYPE=""
+IMAGE_ID=ami-0b4f379183e5706b9
+SECURITY_GROUP_ID=sg-0de06c7e419c50b0c
+
+
 
 for i in "${NAMES[@]}"
 do 
-   echo "NAME: $i"
+   if [ $i == "mongodb" || $i == "mysql" ]
+   then
+        INSTANCE_TYPE="t3.medium"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+   echo "Creating $i instance"
+   aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-groups $SECURITY_GROUP_ID
+   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
+
+#Tag the instance
