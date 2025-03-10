85301c74715e89088110c475df38660d9c202eff
Mahesh <mahi51095@gmail.com>
2025-03-10 19:16:43 +0530

    Added ec2.sh

--- create_instance.sh
+++ create_instance.sh
@@ -7,7 +7,7 @@ SECURITY_GROUP_ID=sg-0de06c7e419c50b0c
 
 
 
-for i in "${NAMES[@]}"
+for i in "${NAMES[@]}";
 do 
    if [ "$i" == "mongodb" ] || [ "$i" == "mysql" ];
    then
@@ -15,12 +15,13 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-   echo "Creating $i instance"
-   aws ec2 run-instances \
+   
+   IP_ADDRESS=$(aws ec2 run-instances \
         --image-id "$IMAGE_ID" \
         --instance-type "$INSTANCE_TYPE" \
         --security-groups "$SECURITY_GROUP_ID" \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "Creating $i instance : $IP_ADDRESS"
 done
 
 #Tag the instance
